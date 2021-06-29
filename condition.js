211f57da6054f7b465a7ed572ed8016bfd2d5aca
Marie-Louise <mariemuteteyimana@gmail.com>
2021-06-29 14:51:28 +0300

    first commit

---
+++ condition.js
@@ -0,0 +1,15 @@
+
+var a=3;
+var b=4;
+var c=a+b;
+console.log(c);
+
+ var anitaB=["Babra","Marie","Zilla","Esther","Tabitha"]
+ var [firstName,secondName,thirdName,fourthName,fifthName]= anitaB
+ console.log(firstName)
+ 
+ var person="Marie";
+ var carName="prado";
+ var price=5666
+ var c=(person+ ""+carName+""+price);
+ console.log(c);
